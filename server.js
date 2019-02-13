b8a560e082a819ea3d4131a48f8bd1924b052362
ethanlam <hellowah5@gmail.com>
2019-02-14 00:43:38 +0800

    add video_id,  /api/marker,    /api/seamarks, bug fixed

--- server.js
+++ server.js
@@ -5,9 +5,115 @@ var express = require('express');
 var app = express();
 var assert = require('assert');
 
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var multer = require('multer');
+var upload = multer();
 
-//api
-app.get('/api', function (req, res) {
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+//form
+app.get('/form', function (req, res) {
+	res.status(200);
+	res.render('postForm');
+})
+
+app.post('/form', upload.array(), (req, res) => {
+	let formData = req.body;
+	console.log('form data', formData);
+	let output = []
+
+	if (isArray(formData.Title)) {
+		for (var i = 0; i < formData.Title.length; i++) {
+			var object = new Object()
+			object.TITLE = formData.Title[i]
+			object.LATITUDE = formData.Lat[i]
+			object.LONGITUDE = formData.Lng[i]
+			object.DESCRIPTION = formData.Description[i]
+			object.VIDEO_ID = formData.VideoNo[i]
+			output.push(object)
+		}
+	} else {
+		var object = new Object()
+		object.TITLE = formData.Title
+		object.LATITUDE = formData.Lat
+		object.LONGITUDE = formData.Lng
+		object.DESCRIPTION = formData.Description
+		object.VIDEO_ID = formData.VideoNo
+		output.push(object)
+	}
+	console.log(output)
+
+	MongoClient.connect(mongourl, function (err, db) {
+		if (err) throw err;
+		db.collection("markers").insert(output, function (err, res) {
+			if (err) throw err
+			console.log("1 document inserted")
+		})
+
+	})
+
+	res.writeHead(200, { "Content-Type": "text/html" })
+	res.write('<html><body>')
+	res.write('<h1>Insert Success!</h1>')
+	res.end('</body></html>')
+});
+
+function isArray(obj) {
+	return !!obj && obj.constructor === Array;
+}
+
+//seamark-api
+app.get('/api/marker', function (req, res) {
+	MongoClient.connect(mongourl, function (err, db) {
+		assert.equal(err, null);
+		db.collection("markers").find().toArray(function (err, docs) {
+			assert.equal(err, null);
+			db.close()
+			if (docs != null) {
+				console.log(docs);
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				docs.forEach(function (doc) {
+					res.write(JSON.stringify(doc, null, '\t'));
+					res.write('\r\n');
+				})
+				res.end();
+			} else {
+				res.sendStatus(404);
+			}
+		})
+
+	})
+});
+
+app.get('/api/marker/:id', function (req, res) {
+	MongoClient.connect(mongourl, function (err, db) {
+		assert.equal(err, null);
+		db.collection("markers").find({"VIDEO_ID":req.params.id}).toArray(function (err, docs) {
+			assert.equal(err, null);
+			db.close()
+			if (docs != null) {
+				console.log(docs);
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				docs.forEach(function (doc) {
+					res.write(JSON.stringify(doc, null, '\t'));
+					res.write('\r\n');
+				})
+				res.end();
+			} else {
+				res.status(404).send("No marker found.");
+			}
+		})
+	})
+});
+
+//seamark-api
+app.get('/api/seamarks', function (req, res) {
 	MongoClient.connect(mongourl, function (err, db) {
 		assert.equal(err, null);
 		db.collection("seamark").find().toArray(function (err, docs) {
@@ -28,7 +134,6 @@ app.get('/api', function (req, res) {
 		})
 
 	})
-}
-)
+});
 
 app.listen(process.env.PORT || 8099);
